67e032e30e84e8c7f6861e56d25e9cd80f998ddf
Milos Bulajic <milosbulajic98@gmail.com>
2025-03-19 13:49:36 +0100

    Match History changes combining into one table

--- nextjs-app/components/matchHistory/showHistory.tsx
+++ nextjs-app/components/matchHistory/showHistory.tsx
@@ -96,54 +96,61 @@ export default function ShowHistory({ matchHistoryList }: { matchHistoryList: Ma
                         </TableHeader>
                         <TableBody>
                             {matchHistoryList.map((match: MatchHistory) => (
-                                <TableRow key={match.id}>
-                                    <TableCell>
-                                        <div className="lg:flex sm:grid sm-grid-column-2 md:grid md-grid-column-2 gap-2 text-center items-center">
-                                            <img src={getHeroImage(match.hero_id)} alt={heroMap[match.hero_id]} width={80} />
-                                            <p>
-                                                {heroToUppercase(heroMap[match.hero_id])}
-                                            </p>
-                                        </div>
-                                    </TableCell>
-                                    <TableCell>{match.winner}</TableCell>
-                                    <TableCell>{formatDuration(match.duration)}</TableCell>
-                                    <TableCell>{match.kills}</TableCell>
-                                    <TableCell>{match.deaths}</TableCell>
-                                    <TableCell>{match.assists}</TableCell>
-                                    <TableCell>
-                                        <div className="lg:grid lg:grid-cols-6 gap-1 md:grid md:grid-cols-3 sm:grid sm:grid-cols-2">
+                                <>
 
-                                            {getItemImage(match.items).map((link: string) => {
-                                                return <img src={link} alt="Item" width={50} />
-                                            })}
-                                        </div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <button
-                                            onClick={() =>
-                                                setShowIframe(showIframe === match.match_id ? null : match.match_id)
-                                            }
-                                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-                                        >
-                                            {showIframe === match.match_id ? "Hide Match" : "Show Match"}
-                                        </button>
-                                    </TableCell>
-                                </TableRow>
+                                    <TableRow key={match.id}>
+                                        <TableCell>
+                                            <div className="lg:flex sm:grid sm-grid-column-2 md:grid md-grid-column-2 gap-2 text-center items-center">
+                                                <img src={getHeroImage(match.hero_id)} alt={heroMap[match.hero_id]} width={80} />
+                                                <p>
+                                                    {heroToUppercase(heroMap[match.hero_id])}
+                                                </p>
+                                            </div>
+                                        </TableCell>
+                                        <TableCell>{match.winner}</TableCell>
+                                        <TableCell>{formatDuration(match.duration)}</TableCell>
+                                        <TableCell>{match.kills}</TableCell>
+                                        <TableCell>{match.deaths}</TableCell>
+                                        <TableCell>{match.assists}</TableCell>
+                                        <TableCell>
+                                            <div className="lg:grid lg:grid-cols-6 gap-1 md:grid md:grid-cols-3 sm:grid sm:grid-cols-2">
+
+                                                {getItemImage(match.items).map((link: string) => {
+                                                    return <img src={link} alt="Item" width={50} />
+                                                })}
+                                            </div>
+                                        </TableCell>
+                                        <TableCell>
+                                            <button
+                                                onClick={() =>
+                                                    setShowIframe(showIframe === match.match_id ? null : match.match_id)
+                                                }
+                                                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+                                            >
+                                                {showIframe === match.match_id ? "Hide Match" : "Show Match"}
+                                            </button>
+                                        </TableCell>
+                                    </TableRow>
+                                    {showIframe === match.match_id && (
+                                        <TableRow>
+                                            <TableCell colSpan={8} className="p-4">
+                                                <iframe
+                                                    src={`https://www.opendota.com/matches/${showIframe}`}
+                                                    width="100%"
+                                                    height="700"
+                                                    className="rounded-lg"
+                                                ></iframe>
+                                            </TableCell>
+                                        </TableRow>
+                                    )}
+                                </>
+
                             ))}
                         </TableBody>
                     </Table>
                 </CardContent>
             </Card>
-            {showIframe && (
-                <div className="mt-4 p-4 border rounded-lg shadow-lg bg-white">
-                    <iframe
-                        src={`https://www.opendota.com/matches/${showIframe}`}
-                        width="100%"
-                        height="700"
-                        className="rounded-lg"
-                    ></iframe>
-                </div>
-            )}
+
         </div>
     )
 }
