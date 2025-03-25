5cc9da929373aeff0d07db8e66160a2d1a39c3ea
WingsDevelopment <bans.cowboy@gmail.com>
2025-03-25 08:23:47 +0100

    minor fixes

--- nextjs-app/app/matchHistory/[id]/page.tsx
+++ nextjs-app/app/matchHistory/[id]/page.tsx
@@ -1,27 +1,9 @@
-import { baseUrl } from "@/app/common/constraints";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableHeaderCell,
-  TableRow,
-} from "@/components/ui/table";
-import { headers } from "next/headers";
-import Link from "next/link";
-import { heroMap, itemMap } from "./hero_and_items_images";
-import { useState } from "react";
-import ShowHistory from "@/components/matchHistory/showHistory";
-import { useSession } from "next-auth/react";
-import { auth, ExtendedUser } from "@/auth";
-import UserProfile from "@/components/userProfile/userProfile";
+import { baseUrl } from '@/app/common/constraints';
+import { headers } from 'next/headers';
+import { heroMap, itemMap } from './hero_and_items_images';
+import ShowHistory from '@/components/matchHistory/showHistory';
+import { auth, ExtendedUser } from '@/auth';
+import UserProfile from '@/components/userProfile/userProfile';
 
 export interface MatchHistoryProps {
   params: {
@@ -38,7 +20,7 @@ export interface MatchHistory {
   game_mode: string;
   lobby_type: string;
   region: string;
-  winner: "radiant" | "dire"; // Enforce possible values
+  winner: 'radiant' | 'dire'; // Enforce possible values
   radiant_score: number;
   dire_score: number;
   additional_info: string; // JSON string, might need parsing
@@ -52,15 +34,15 @@ export default async function MatchHistory({ params }: MatchHistoryProps) {
   const session = await auth();
   const discordId = (session?.user as ExtendedUser)?.discordId;
   const { id } = params;
-  const cookie = headers().get("cookie") || "";
+  const cookie = headers().get('cookie') || '';
   // todo: we should fetcher instead
   const [matchHistoryRes, isPublicProfile] = await Promise.all([
     fetch(`${baseUrl}/api/match-history-players/show-history?steam_id=${id}`, {
-      cache: "no-store",
+      cache: 'no-store',
       headers: { cookie },
     }),
     fetch(`${baseUrl}/api/player/is_public_profile?steam_id=${id}`, {
-      cache: "no-store",
+      cache: 'no-store',
       headers: { cookie },
     }),
   ]);
