b7cbd24d765f60595197d6f09b488ece044c8417
WingsDevelopment <bans.cowboy@gmail.com>
2025-03-25 23:39:43 +0100

    update fab.py

--- python-scripts/free_all_bots.py
+++ python-scripts/free_all_bots.py
@@ -1,16 +1,15 @@
 import sqlite3
 import os
 
-
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 DB_PATH = os.getenv("DATABASE_PATH", os.path.join(BASE_DIR, "db", "league.db"))
 
 conn = sqlite3.connect(DB_PATH, uri=True)
 cursor = conn.cursor()
 
-# Update the SteamBots table to set status=0 for the bot with id 1
-cursor.execute("UPDATE SteamBots SET status = 0 WHERE id = ?", (1,))
+# Update all rows in SteamBots table to set status = 0
+cursor.execute("UPDATE SteamBots SET status = 0")
 conn.commit()
 conn.close()
 
-print("Bot status updated to 0.")
+print("All bots status updated to 0.")
