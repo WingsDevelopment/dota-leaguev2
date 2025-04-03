160f9f03372a3a2e9638c33542c5088a5dc4ec9c
WingsDevelopment <bans.cowboy@gmail.com>
2025-04-04 00:04:26 +0200

    added some more comments

--- nextjs-app/app/admin/reports/page.tsx
+++ nextjs-app/app/admin/reports/page.tsx
@@ -1,7 +1,15 @@
 import ReportsTableContainer from "@/app/admin/reports/components/reports-table-container";
 import { apiCallerGetReports } from "../../api/report-system/get-reports/caller";
 
+/* -------------------- */
+/*   Server Component   */
+/* -------------------- */
+
 export default async function Page() {
+  /* ------- */
+  /*   JSX   */
+  /* ------- */
+
   return (
     <div className="flex flex-col gap-4">
       <ReportsTableContainer reportList={await apiCallerGetReports()} />
