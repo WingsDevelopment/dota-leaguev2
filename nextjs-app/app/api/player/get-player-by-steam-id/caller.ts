f9d89f9c793b123ee9e213e8b4a16c834d0632f0
WingsDevelopment <bans.cowboy@gmail.com>
2025-04-08 23:10:18 +0200

    import fix

--- nextjs-app/app/api/player/get-player-by-steam-id/caller.ts
+++ nextjs-app/app/api/player/get-player-by-steam-id/caller.ts
@@ -1,21 +1,20 @@
 import axios from "axios";
-import type { PrimitiveServiceResponse } from "../../../services/common/types";
-import { Leaderboard } from "@/app/services/playerService/importPlayers";
-import { getPlayerBySteamId } from "@/app/services/playerService/getPlayerBySteamId";
+import {
+  getPlayerBySteamId,
+  Player,
+} from "@/app/services/playerService/getPlayerBySteamId";
 import { baseUrl } from "@/app/common/constraints";
-import { Player } from "@/components/admin/player-crud";
 
-export const apiCallerGetPlayerBySteamId = async (
-    {steamId}:getPlayerBySteamId
-): Promise<Player> => {
+export const apiCallerGetPlayerBySteamId = async ({
+  steamId,
+}: getPlayerBySteamId): Promise<Player> => {
   try {
     const response = await axios.get(`${baseUrl}/api/player/get-player-by-steam-id`, {
-        params:{steamId}
+      params: { steamId },
     });
 
-    const data = response.data
+    const data = response.data;
     if (!data.success) throw new Error(data.message);
-    console.log(data,'data')
     return data.data;
   } catch (error) {
     console.error("Failed to get player by steam Id!", error);
--- nextjs-app/app/services/playerService/getPlayerBySteamId.ts
+++ nextjs-app/app/services/playerService/getPlayerBySteamId.ts
@@ -1,30 +1,35 @@
 import { getDbInstance } from "@/db/utils";
 import { closeDatabase } from "@/db/initDatabase";
 import { NextResponse } from "next/server";
-import { getPrimitiveServiceErrorResponse, getSuccessfulServiceResponse, runDbAll } from "../common/functions";
+import {
+  getPrimitiveServiceErrorResponse,
+  getSuccessfulServiceResponse,
+  runDbAll,
+} from "../common/functions";
+import { ServiceResponse } from "../common/types";
 /* --------- */
 /*   Types   */
 /* --------- */
 export interface getPlayerBySteamId {
-    steamId: string | null;
+  steamId: string | null;
 }
 export interface Player {
-    id: number;
-    discord_id: number;
-    steam_id: number;
-    name: string;
-    mmr: number;
-    captain: number;
-    banned_until: string;
-    games_didnt_show: number;
-    games_left: number;
-    games_griefed: number;
-    bbb: number;
-    is_public_profile: boolean;
-    wins: number | null;
-    loses: number | null;
-    streak: number | null;
-    vouched_date: string | null;
+  id: number;
+  discord_id: number;
+  steam_id: number;
+  name: string;
+  mmr: number;
+  captain: number;
+  banned_until: string;
+  games_didnt_show: number;
+  games_left: number;
+  games_griefed: number;
+  bbb: number;
+  is_public_profile: boolean;
+  wins: number | null;
+  loses: number | null;
+  streak: number | null;
+  vouched_date: string | null;
 }
 /**
  * Gets the player by steam ID.
@@ -32,46 +37,52 @@ export interface Player {
  * @async
  * @function getPlayerBySteamId
  * @param {getPlayerBySteamId} params - The object containing the steam ID identifier.
- * @returns {Promise<PrimitiveServiceResponse>} A promise that resolves to a primitive service response.
+ * @returns {Promise<ServiceResponse<Player | undefined>>} A promise that resolves to a primitive service response.
  *
  * @example
  * const response = await getPlayerBySteamId({ steamId: 12345 });
  */
-export async function getPlayerBySteamId<Promise>({ steamId }: getPlayerBySteamId) {
-    /* ------------------ */
-    /*   Initialization   */
-    /* ------------------ */
-    const db = await getDbInstance();
-    try {
-        /* ------------- */
-        /*   Validation  */
-        /* ------------- */
-        if (!steamId) {
-            throw new Error("Missing required field: steam_id")
-        }
-        /* ------------- */
-        /*   DB Query    */
-        /* ------------- */
-        const players = await runDbAll<Player[]>(db, `SELECT * FROM Players WHERE steam_id = ?`, [steamId]);
-        /* ---------------- */
-        /*   Return Data    */
-        /* ---------------- */
-        if (players.length > 1) {
-            console.log("There is more than two players")
-        }
-        return getSuccessfulServiceResponse({
-            message: "Fetched player by steam id successfully.",
-            data: players[0]
-        });
-    } catch (error) {
-        /* -------- */
-        /*   Error  */
-        /* -------- */
-        return getPrimitiveServiceErrorResponse(error, "Error finding player.");
-    } finally {
-        /* -------- */
-        /*  Cleanup */
-        /* -------- */
-        closeDatabase(db)
+export async function getPlayerBySteamId({
+  steamId,
+}: getPlayerBySteamId): Promise<ServiceResponse<Player | undefined>> {
+  /* ------------------ */
+  /*   Initialization   */
+  /* ------------------ */
+  const db = await getDbInstance();
+  try {
+    /* ------------- */
+    /*   Validation  */
+    /* ------------- */
+    if (!steamId) {
+      throw new Error("Missing required field: steam_id");
     }
+    /* ------------- */
+    /*   DB Query    */
+    /* ------------- */
+    const players = await runDbAll<Player[]>(
+      db,
+      `SELECT * FROM Players WHERE steam_id = ?`,
+      [steamId]
+    );
+    /* ---------------- */
+    /*   Return Data    */
+    /* ---------------- */
+    if (players.length > 1) {
+      console.log("There is more than two players");
+    }
+    return getSuccessfulServiceResponse({
+      message: "Fetched player by steam id successfully.",
+      data: players[0],
+    });
+  } catch (error) {
+    /* -------- */
+    /*   Error  */
+    /* -------- */
+    return getPrimitiveServiceErrorResponse(error, "Error finding player.");
+  } finally {
+    /* -------- */
+    /*  Cleanup */
+    /* -------- */
+    closeDatabase(db);
+  }
 }
