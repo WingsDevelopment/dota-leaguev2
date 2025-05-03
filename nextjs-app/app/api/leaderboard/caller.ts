1a936e0c90d8935ca64764b019adf32dfb61ae6c
Milos Bulajic <milosbulajic98@gmail.com>
2025-05-03 20:29:30 +0200

    refactored leaderboard
    Added caller pages for games-crud and game-history (no code in them yet)

---
+++ nextjs-app/app/api/leaderboard/caller.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { getBaseUrl } from "@/app/common/constraints";
+import { ApiCallerConfig } from "../common/interfaces";
+import { PrimitiveServiceResponse } from "@/app/services/common/types";
+import { Player } from "@/app/services/playerService/getPlayerBySteamId";
+
+
+export const apiCallerGetLeaderBoard = async ({
+    config
+}: { config: ApiCallerConfig }): Promise<Player[]> => {
+    try {
+        const response = await axios.get(`${getBaseUrl(config?.origin)}/api/leaderboard`, {
+            params: { config }
+        });
+
+        const data = response.data;
+        if (!data.success) throw new Error(data.message);
+        return data.data;
+    } catch (error) {
+        console.error(`Failed to get leaderboard!`, error);
+        throw error;
+    }
+};
